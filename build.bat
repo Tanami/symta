13a1a07a164df2fad9bd7d36f038d1b06273a0e3
saniv <snv985@gmail.com>
2014-10-20 22:51:44 +0400

    replaced broken release

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 @echo off
 set cflags=-O1 -Wno-return-type -Wno-pointer-sign -D WINDOWS -I ./runtime
 set bs=bootstrap/c/
-gcc %cflags% -I ./runtime/w runtime/runtime.c runtime/w/dlfcn.c runtime/w/compat.c -o run.exe
+gcc %cflags% -I ./runtime/w runtime/runtime.c runtime/w/dlfcn.c runtime/w/compat.c -o symta.exe
 mkdir lib
-gcc %cflags% -fpic -shared %bs%core_.c -o lib/core_.dll
-gcc %cflags% -fpic -shared %bs%reader.c -o lib/reader.dll
-gcc %cflags% -fpic -shared %bs%compiler.c -o lib/compiler.dll
-gcc %cflags% -fpic -shared %bs%macro.c -o lib/macro.dll
-gcc %cflags% -fpic -shared %bs%eval.c -o lib/eval.dll
-gcc %cflags% -fpic -shared %bs%main.c -o lib/main.dll
+gcc %cflags% -fpic -shared %bs%core_.c -o lib/core_.
+gcc %cflags% -fpic -shared %bs%reader.c -o lib/reader.
+gcc %cflags% -fpic -shared %bs%compiler.c -o lib/compiler.
+gcc %cflags% -fpic -shared %bs%macro.c -o lib/macro.
+gcc %cflags% -fpic -shared %bs%eval.c -o lib/eval.
+gcc %cflags% -fpic -shared %bs%main.c -o lib/main.
