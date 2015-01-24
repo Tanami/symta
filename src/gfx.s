52d7d49f6618e06e617fe0e85669019e12d257ad
saniv <snv985@gmail.com>
2015-01-24 11:44:59 +0400

    optimizations

--- src/gfx.s
+++ src/gfx.s
@@ -15,14 +15,14 @@ new_cmap Xs =
 
 type widget
 
-type gfx.widget.no_copy{@As} handle
+type gfx.widget.no_copy{@As} handle w h
 | $handle <= case As
   [W<1.is_int H<1.is_int] | new_gfx_ W H
   [Filename<1.is_text] | gfx_load_png Filename
   Else | bad "cant create gfx from [As]"
+| $w <= gfx_w $handle
+| $h <= gfx_h $handle
 gfx.free = free_gfx $handle
-gfx.w = gfx_w $handle
-gfx.h = gfx_h $handle
 gfx.hotspot = [(gfx_hotspot_x $handle) (gfx_hotspot_y $handle)]
 gfx.`!hotspot` [X Y] = gfx_set_hotspot $handle X Y
 gfx.get X Y = gfx_get $handle X Y
@@ -32,7 +32,9 @@ gfx.line Color A B = gfx_line $handle Color A.0 A.1 B.0 B.1
 gfx.rect Color Fill X Y W H = gfx_rect $handle Color Fill X Y W H
 gfx.circle Color Fill C R = gfx_circle $handle Color Fill C.0 C.1 R
 gfx.triangle Color A B C = gfx_triangle $handle Color A.0 A.1 B.0 B.1 C.0 C.1
-gfx.resize W H = gfx_resize $handle W H
+gfx.resize W H =
+| bad "FIXME: gfx.resize"
+| gfx_resize $handle W H
 gfx.save Filename = gfx_save_png Filename $handle
 gfx.cmap raw/0 =
 | P = gfx_cmap $handle
@@ -47,8 +49,11 @@ gfx.blit P Src rect/0 flipX/0 flipY/0 map/0 =
 | less Src.is_gfx:
   | Src.draw{Me P}
   | leave 0
-| [SX SY SW SH] = if Rect then Rect else [0 0 Src.w Src.h]
-| gfx_blit $handle P.0 P.1 Src.handle SX SY SW SH FlipX FlipY Map
+| _type gfx Src: if Rect
+  then gfx_blit $handle P.0 P.1 Src.handle Rect.0 Rect.1 Rect.2 Rect.3 FlipX FlipY Map
+  else gfx_blit $handle P.0 P.1 Src.handle 0 0 Src.w Src.h FlipX FlipY Map
+gfx.blitRaw X Y Src = _type gfx Src
+| gfx_blit $handle X Y Src.handle 0 0 Src.w Src.h 0 0 0
 gfx.margins =
 | P = gfx_margins $handle
 | [(_ffi_get uint32_t P 0) (_ffi_get uint32_t P 1)
