3d761f17ae4676881e7e7ec4ac0b8d98633bea3a
saniv <snv985@gmail.com>
2015-05-18 06:38:41 +0400

    bits.clear fix

--- src/bits.s
+++ src/bits.s
@@ -1,16 +1,15 @@
 type bits{size} xs
 | $xs <= (($size+7)/8).bytes
 
-bits.clear =
-| for I $xs.size: $xs.I <= 0
-
+bits.clear Bit =
+| Byte = if Bit then #FF else 0
+| for I $xs.size: $xs.I <= Byte
 
 bits.`.`  N = $xs.(N/>3).get{N^^7}
 
 bits.`!`  N V =
 | I = N/>3
 | $xs.I <= $xs.I.set{N^^7 V}
-
 
 bits.list = map I $size $I
 
