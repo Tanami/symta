d63507d6c081ec5077dc41f457d937f80371ada4
saniv <snv985@gmail.com>
2015-06-28 12:08:17 +0300

    follow up

---
+++ src/ecs.s
@@ -0,0 +1,13 @@
+use ecs_
+
+
+component n0_
+/*component n1_
+component n2_
+component n3_
+component n4_
+component n5_
+component n6_
+component n7_*/
+
+export ecs ecs_register ecs_array_get ecs_array_set component_ 'component'
